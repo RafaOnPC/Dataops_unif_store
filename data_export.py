d708217dcacd9cfd971b067e1bfce9307f8a42ee
Rafa <YamiYugi2223@outlook.com>
2026-02-05 01:28:50 -0500

    Fixing code for ETL Process

--- data_export.py
+++ data_export.py
@@ -2,29 +2,27 @@ import os
 import pandas as pd
 
 
-def extraer_mes(nombre_archivo):
-    # Extraccion del mes
-    nombre = os.path.splitext(nombre_archivo)[0]
-    partes = nombre.split('_')
-    mes = partes[-1]
-    return mes.title()
-
-
 def exportar_datos(df):
     # Crear carpeta output si no existe
     os.makedirs('output', exist_ok=True)
 
     archivo_global = os.path.join('output', 'Fich_Ventas_GLOBAL.xlsx')
 
-    # Exportacion por Mes agrupacion por columna Archivo_origen
-    for archivo_origen, df_mes in df.groupby('archivo_origen'):
-        mes = extraer_mes(archivo_origen)
-        archivo_excel = os.path.join('output',f'Fich_Ventas_{mes}.xlsx')
+    # Exportacion de archivos por mes
+    for mes, df_mes in df.groupby('mes'):
+        mes_archivo = mes.capitalize()
+        archivo_excel = os.path.join(
+            'output',
+            f'Fich_Ventas_{mes_archivo}.xlsx'
+        )
+
+        # Eliminanacio de columna tecnica
         df_export = df_mes.drop(columns=['archivo_origen'], errors='ignore')
+
         df_export.to_excel(archivo_excel, index=False)
         print(f'Archivo generado: {archivo_excel}')
 
-    # Creacion de archivo global
+    # 2. Archivo global historico
     df_nuevo = df.drop(columns=['archivo_origen'], errors='ignore')
 
     if os.path.exists(archivo_global):
